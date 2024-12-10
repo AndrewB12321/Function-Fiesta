2fe673f372a56349f2f47b3663138ee94f5ea276
Andrew <andrewboland0@gmail.com>
2024-12-09 22:07:23 -0500

    added changing button colors

--- Assets/MainMenuAssets/Scripts/ButtonManager.cs
+++ Assets/MainMenuAssets/Scripts/ButtonManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -8,9 +9,45 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] private GameObject levelSelectMenu;
     [SerializeField] private GameObject creditsScreen;
 
+    [SerializeField] private GameObject level1;
+    [SerializeField] private GameObject level2;
+    [SerializeField] private GameObject level3;
+    [SerializeField] private GameObject level4;
+    [SerializeField] private GameObject level5;
+    [SerializeField] private GameObject level6;
+
     private void Start()
     {
         BackButton();
+    }
+
+    private void Update()
+    {
+        Image level1Renderer = level1.GetComponent<Image>();
+        Image level2Renderer = level2.GetComponent<Image>();
+        Image level3Renderer = level3.GetComponent<Image>();
+        Image level4Renderer = level4.GetComponent<Image>();
+        Image level5Renderer = level5.GetComponent<Image>();
+        Image level6Renderer = level6.GetComponent<Image>();
+
+        level1Renderer.color = getColor(1, PlayerPrefs.GetInt("LevelUnlocked"));
+        level2Renderer.color = getColor(2, PlayerPrefs.GetInt("LevelUnlocked"));
+        level3Renderer.color = getColor(3, PlayerPrefs.GetInt("LevelUnlocked"));
+        level4Renderer.color = getColor(4, PlayerPrefs.GetInt("LevelUnlocked"));
+        level5Renderer.color = getColor(5, PlayerPrefs.GetInt("LevelUnlocked"));
+        level6Renderer.color = getColor(6, PlayerPrefs.GetInt("LevelUnlocked"));
+    }
+
+    private Color getColor(int level, int playerLevel)
+    {
+        if(level > playerLevel)
+        {
+            return Color.red;
+        }
+        else
+        {
+            return Color.yellow;
+        }
     }
 
     public void LevelSelectButton()
@@ -19,6 +56,8 @@ public class ButtonManager : MonoBehaviour
         settingsMenu.SetActive(false);
         levelSelectMenu.SetActive(true);
         creditsScreen.SetActive(false);
+        PlayerPrefs.SetInt("levelUnlocked", 1);
+        Debug.Log(PlayerPrefs.GetInt("levelUnlocked"));
     }
 
     public void SettingsButton()
