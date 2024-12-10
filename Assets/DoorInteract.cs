3e6362fc7468eb95259d17b406ce123dcd51f0d5
Andrew <andrewboland0@gmail.com>
2024-12-09 21:32:09 -0500

    Update DoorInteract.cs
    door can now only be opened if keys are picked up

--- Assets/DoorInteract.cs
+++ Assets/DoorInteract.cs
@@ -3,18 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class DoorInteract : MonoBehaviour
 {
+    public GameObject[] keys;
+    bool open;
+    private void Start()
+    {
+        keys = GameObject.FindGameObjectsWithTag("Key");
+        open = false;
+    }
+    private void Update()
+    {
+        keys = GameObject.FindGameObjectsWithTag("Key");
+        if(keys.Length == 0)
+            open = true;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            int currentLevel = PlayerPrefs.GetInt("LevelUnlocked");
+            if(open){
+                int currentLevel = PlayerPrefs.GetInt("LevelUnlocked");
 
-            if (currentLevel == 0)
-                PlayerPrefs.SetInt("LevelUnlocked", 1);
+                if (currentLevel == 0)
+                    PlayerPrefs.SetInt("LevelUnlocked", 1);
 
-            PlayerPrefs.SetInt("LevelUnlocked", currentLevel + 1);
-            Debug.Log(PlayerPrefs.GetInt("LevelUnlocked"));
-            SceneManager.LoadScene(0);
+                PlayerPrefs.SetInt("LevelUnlocked", currentLevel + 1);
+                Debug.Log(PlayerPrefs.GetInt("LevelUnlocked"));
+                SceneManager.LoadScene(0);
+            }
         }
     }
 }
