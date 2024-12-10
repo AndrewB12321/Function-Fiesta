b649d9e39899926881756f72f8d281cafa9b8194
William Fondoulis <sm.wfondu@gmail.com>
2024-12-09 21:09:08 -0500

    Fix key

--- Assets/KeyInteract.cs
+++ Assets/KeyInteract.cs
@@ -3,23 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class KeyInteract : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
